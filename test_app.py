091722420669b9f83304f6d68dbded8cdf6f6c70
san90279 <san90279@gmail.com>
2024-03-19 16:45:24 +0800

    unit test finish

---
+++ test_app.py
@@ -0,0 +1,29 @@
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.get('/tasks')
+    
+    def test_get_tasks(self):
+        response = self.client.get('/tasks')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_task(self):
+        task_data = {'text': 'New task'}
+        response = self.client.post('/task', json=task_data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_update_task(self):
+        task_data = {'text': 'New task',"status": 1,"id": 1}
+        response = self.client.put('/task', json=task_data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_task(self):
+        response = self.client.delete('/task/1')
+        self.assertEqual(response.status_code, 200)        
+
+if __name__ == '__main__':
+    unittest.main()
